28a15e446b98d9682e513607cb0a19fea786155d
neilsatt <neilsatt@gmail.com>
2017-07-07 18:34:49 -0600

    update after compressing css and js

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,12 @@ var gulp = require('gulp'),
   uglify = require('gulp-uglify');
   
 gulp.task("concatScripts", function() {
-  gulp.src([
-      'js/fastclick.js', 
+  gulp.src([  
+      'js/fastclick.js',
       'js/foundatation.js',
       'js/foundation.equalizer.js',
       'js/foundation.reveal.js',
-      'js/jquery.js',
-      'js/scripts.js'])
+      ])
      .pipe(concat("app.js")) // which file will hold all concatenated files content
      .pipe(gulp.dest("js")); // folder that holds app.js file
 });
